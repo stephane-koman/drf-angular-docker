d1b2c8ec8ad143935c23b28c65407f836432629a
Stephane Koman <stefchris2@gmail.com>
2019-12-14 19:02:23 +0100

    Create Task model and generate dummy data

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+from rest_framework import generics
+
+class TaskList(generics.ListAPIView):
+    """
+    View all tasks.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
